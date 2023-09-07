0cfdccf9489c152295fa6c2e8ae309fc892de356
AlexeySmirnov93 <voinvetra92@mail.ru>
2023-09-07 17:38:06 +0300

    ДЗ_15

--- Homework_14/unittest/unit_test.py
+++ Homework_14/unittest/unit_test.py
@@ -6,21 +6,24 @@ from func_fib import fib
 RESULT = [0, 1, 1, 2, 3]
 
 class TestFuncFib(unittest.TestCase):
+
     def test_expected_result(self):
-        self.assertEqual(list(fib(5)), RESULT)
+            self.assertEqual(list(fib(5)), RESULT)
 
     def test_type_float(self):
-        self.assertRaises(TypeError, "'float' object cannot be interpreted as an integer", fib, 5.1 )
+            self.assertRaises(TypeError, "'float' object cannot be interpreted as an integer", fib, 5.1 )
 
     def test_type_str(self):
-        self.assertRaises(TypeError, "'<' not supported between instances of 'str' and 'int'", fib, 's') 
-
+            self.assertRaises(TypeError, "'<' not supported between instances of 'str' and 'int'", fib, 's') 
+            
     def test_valie_negative_number(self):
-        self.assertRaises(ValueError, "The number n must be greater than 2", fib, -5)
-        
+            with self.assertRaises(ValueError):
+                list(fib(-5))
+                   
     def test_valie_border_number(self):
-        self.assertRaises(ValueError, "The number n must be greater than 2", fib, 1)
+            with self.assertRaises(ValueError):
+                list(fib(1))
+                   
         
-
 if __name__ == "__main__":
     unittest.main(verbosity=10)
---
+++ Homework_15/task_1.py
@@ -0,0 +1,47 @@
+import logging
+
+FORMAT = '{levelname} - {asctime} {msg}'
+
+logging.basicConfig(filename='student.log', encoding='utf-8', level=logging.INFO, filemode='a', format=FORMAT, style='{')
+
+class Name_check:
+
+    def __set_name__(self, owner, name):
+        self._name = "_" + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self._name)
+
+    def __set__(self, instance, value):
+        if not value.isalpha():
+            error_message = 'ФИО должно содержать только буквы'
+            logging.error(f'{error_message}. Введенное значение: {value}')
+            raise ValueError(error_message)
+        if not value.istitle():
+            error_message = 'ФИО должно быть с заглавной буквы'
+            logging.error(f'{error_message}. Введенное значение: {value}')
+            raise ValueError(error_message)
+        setattr(instance, self._name, value)
+
+class Student:
+
+    first_name = Name_check()
+    middle_name = Name_check()
+    last_name = Name_check()
+
+    def __init__(self, last_name, first_name, middle_name):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.middle_name = middle_name
+        logging.info('Объект Student успешно создан')
+        logging.info(f'Введенные данные: Фамилия: {last_name}, Имя: {first_name}, Отчество: {middle_name}')
+
+    def __repr__(self) -> str:
+        return f'ФИО: {self.last_name} {self.first_name} {self.middle_name}'
+
+if __name__ == '__main__':
+    try:
+        name = Student('Смирнов', 'Алексей', 'Михайлович')
+        print(name)
+    except ValueError as e:
+        logging.error(f'Произошла ошибка: {str(e)}')
